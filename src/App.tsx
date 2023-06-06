f01782b56f78bf6ad87bc4e5ad329e84bc66cb97
Alex Vido <alex.vido93@gmail.com>
2023-06-06 15:12:16 -0300

    Exercise 3 with indentation problem in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const [newPassword, setNewPassword] = useState(false);
+
+  const formNewPassword = () => {
+    setNewPassword(true);
+  };
+
+  const cancelNewPassword = () => {
+    setNewPassword(false);
+  };
+
   return (
     <div>
       <h1>Gerenciador de senhas</h1>
-      <Form />
+      {
+        newPassword
+          ? <Form cancelNewPassword={ cancelNewPassword } />
+          : <button id="newPassword" onClick={ formNewPassword }>
+            Cadastrar nova senha
+            </button>
+      }
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,20 @@
-function Form() {
+type FormProps = {
+  cancelNewPassword: () => void
+};
+
+function Form({ cancelNewPassword }: FormProps) {
   return (
     <form>
       <label htmlFor="serviceName">Nome do serviço</label>
       <input type="text" id="serviceName" />
-      <label htmlFor="login">Login</label>
-      <input type="text" id="login" />
+      <label htmlFor="Login">Login</label>
+      <input type="text" id="Login" />
       <label htmlFor="password">Senha</label>
       <input type="password" id="password" />
-      <label htmlFor="url">URL</label>
-      <input type="text" id="url" />
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
+      <label htmlFor="URL">URL</label>
+      <input type="text" id="URL" />
+      <button id="Cadastrar" onClick={ cancelNewPassword }>Cadastrar:</button>
+      <button id="Cancelar" onClick={ cancelNewPassword }>Cancelar:</button>
     </form>
   );
 }
